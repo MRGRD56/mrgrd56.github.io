abcda3bc609df0eb5438209049462d56e89b19f5
k.golikov <kgolikov@37bytes.com>
2022-04-02 01:01:50 +0500

    Make logo a link

--- src/layouts/appLayout/components/appHeader/AppHeader.tsx
+++ src/layouts/appLayout/components/appHeader/AppHeader.tsx
@@ -3,14 +3,18 @@ import { Header } from 'antd/lib/layout/layout';
 import styles from './AppHeader.module.scss';
 import Text from 'antd/lib/typography/Text';
 import AppHeaderSearch from './components/appHeaderSearch/AppHeaderSearch';
+import { Link } from 'react-router-dom';
+import { routes } from '../../../../constants/router/routes';
 
 const AppHeader = () => {
     return (
         <Header className={styles.container}>
             <div className={styles.logo}>
-                <Text strong className={styles.logoText}>
-                    MRGRD56
-                </Text>
+                <Link to={routes.root.path}>
+                    <Text strong className={styles.logoText}>
+                        MRGRD56
+                    </Text>
+                </Link>
             </div>
             {/*<Menu theme="dark"></Menu>*/}
             <AppHeaderSearch />
