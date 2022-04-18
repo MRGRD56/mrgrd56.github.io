afb2fba938d441142db52229bb9815a09c4212d3
k.golikov <kgolikov@37bytes.com>
2022-04-18 23:22:45 +0500

    Quotes

--- src/utils/scopedEval.ts
+++ src/utils/scopedEval.ts
@@ -2,7 +2,7 @@ const scopedEval = (expression: string, context: object) => {
     const evaluator = Function.apply(null, [
         ...Object.keys(context),
         'expression',
-        "return eval('expression = undefined;' + expression)"
+        'return eval("expression = undefined;" + expression)'
     ]);
     return evaluator.apply(null, [...Object.values(context), expression]);
 };
