3e9d001d80a9dc5974bcea3b03b11cd9adf7d140
k.golikov <kgolikov@37bytes.com>
2022-05-10 20:05:41 +0500

    HtmlEditorPage: add react-split

--- src/pages/htmlEditorPage/HtmlEditorPage.tsx
+++ src/pages/htmlEditorPage/HtmlEditorPage.tsx
@@ -17,6 +17,8 @@ import { editor } from 'monaco-editor';
 import ButtonGroup from 'antd/lib/button/button-group';
 import { VerticalSplit, ViewHeadline } from '@mui/icons-material';
 import useChangeValueStateHandler from '../../hooks/useChangeValueStateHandler';
+import Split from 'react-split';
+import './HtmlEditorPage.scss';
 
 enum EditorTab {
     HTML = 'html',
@@ -126,113 +128,124 @@ ${sources.js}
         emmetJSX(monaco);
     }, []);
 
-    const leftColSpan = {
-        [ViewMode.EDITOR]: 24,
-        [ViewMode.SPLIT]: 12,
-        [ViewMode.VIEW]: 0
-    }[viewMode];
+    // const leftColSpan = {
+    //     [ViewMode.EDITOR]: 24,
+    //     [ViewMode.SPLIT]: 12,
+    //     [ViewMode.VIEW]: 0
+    // }[viewMode];
+    //
+    // const rightColSpan = {
+    //     [ViewMode.EDITOR]: 0,
+    //     [ViewMode.SPLIT]: 12,
+    //     [ViewMode.VIEW]: 24
+    // }[viewMode];
 
-    const rightColSpan = {
-        [ViewMode.EDITOR]: 0,
-        [ViewMode.SPLIT]: 12,
-        [ViewMode.VIEW]: 24
-    }[viewMode];
+    const leftCol = (
+        <Col className={classNames(styles.col, styles.leftCol, { 'd-none': viewMode === ViewMode.VIEW })}>
+            <Tabs
+                activeKey={editorTab}
+                onChange={handleEditorTabChange}
+                tabBarStyle={tabBarStyle}
+                className={styles.editorTabs}
+                tabBarGutter={10}
+                tabBarExtraContent={
+                    <ButtonGroup>
+                        <Button
+                            type="text"
+                            icon={<ViewHeadline />}
+                            className={classNames({ 'antd-text-primary': viewMode === ViewMode.EDITOR })}
+                            onClick={handleViewModeChange(ViewMode.EDITOR)}
+                        />
+                        <Button
+                            type="text"
+                            icon={<VerticalSplit />}
+                            className={classNames({ 'antd-text-primary': viewMode === ViewMode.SPLIT })}
+                            onClick={handleViewModeChange(ViewMode.SPLIT)}
+                        />
+                    </ButtonGroup>
+                }
+            >
+                <Tabs.TabPane
+                    tab={
+                        <div className={styles.tabTitleWrapper}>
+                            <Html className={styles.tabTitle} />
+                        </div>
+                    }
+                    key={EditorTab.HTML}
+                    className={styles.editorTab}
+                >
+                    <AppEditor
+                        value={sources.html}
+                        onChange={handleSourceChange(EditorTab.HTML)}
+                        className={styles.editor}
+                        language="html"
+                        onMount={handleEditorMount}
+                        options={editorOptions}
+                    />
+                </Tabs.TabPane>
+                <Tabs.TabPane
+                    tab={
+                        <div className={styles.tabTitleWrapper}>
+                            <Css className={styles.tabTitle} />
+                        </div>
+                    }
+                    key={EditorTab.CSS}
+                    className={styles.editorTab}
+                >
+                    <AppEditor
+                        value={sources.css}
+                        onChange={handleSourceChange(EditorTab.CSS)}
+                        className={styles.editor}
+                        language="css"
+                        onMount={handleEditorMount}
+                        options={editorOptions}
+                    />
+                </Tabs.TabPane>
+                <Tabs.TabPane
+                    tab={
+                        <div className={styles.tabTitleWrapper}>
+                            <Javascript className={styles.tabTitle} />
+                        </div>
+                    }
+                    key={EditorTab.JS}
+                    className={styles.editorTab}
+                >
+                    <AppEditor
+                        value={sources.js}
+                        onChange={handleSourceChange(EditorTab.JS)}
+                        className={styles.editor}
+                        language="javascript"
+                        onMount={handleEditorMount}
+                        options={editorOptions}
+                    />
+                </Tabs.TabPane>
+            </Tabs>
+        </Col>
+    );
+
+    const rightCol = (
+        <Col
+            className={classNames('yui3-cssreset', styles.col, styles.rightCol, {
+                'd-none': viewMode === ViewMode.EDITOR
+            })}
+        >
+            <iframe srcDoc={resultSource} className={styles.resultFrame} />
+        </Col>
+    );
 
     return (
         <PageContainer noPadding className={styles.container}>
-            <Row className={styles.containerRow}>
-                <Col
-                    span={leftColSpan}
-                    className={classNames(styles.col, styles.leftCol, { 'd-none': viewMode === ViewMode.VIEW })}
-                >
-                    <Tabs
-                        activeKey={editorTab}
-                        onChange={handleEditorTabChange}
-                        tabBarStyle={tabBarStyle}
-                        className={styles.editorTabs}
-                        tabBarGutter={10}
-                        tabBarExtraContent={
-                            <ButtonGroup>
-                                <Button
-                                    type="text"
-                                    icon={<ViewHeadline />}
-                                    className={classNames({ 'antd-text-primary': viewMode === ViewMode.EDITOR })}
-                                    onClick={handleViewModeChange(ViewMode.EDITOR)}
-                                />
-                                <Button
-                                    type="text"
-                                    icon={<VerticalSplit />}
-                                    className={classNames({ 'antd-text-primary': viewMode === ViewMode.SPLIT })}
-                                    onClick={handleViewModeChange(ViewMode.SPLIT)}
-                                />
-                            </ButtonGroup>
-                        }
-                    >
-                        <Tabs.TabPane
-                            tab={
-                                <div className={styles.tabTitleWrapper}>
-                                    <Html className={styles.tabTitle} />
-                                </div>
-                            }
-                            key={EditorTab.HTML}
-                            className={styles.editorTab}
-                        >
-                            <AppEditor
-                                value={sources.html}
-                                onChange={handleSourceChange(EditorTab.HTML)}
-                                className={styles.editor}
-                                language="html"
-                                onMount={handleEditorMount}
-                                options={editorOptions}
-                            />
-                        </Tabs.TabPane>
-                        <Tabs.TabPane
-                            tab={
-                                <div className={styles.tabTitleWrapper}>
-                                    <Css className={styles.tabTitle} />
-                                </div>
-                            }
-                            key={EditorTab.CSS}
-                            className={styles.editorTab}
-                        >
-                            <AppEditor
-                                value={sources.css}
-                                onChange={handleSourceChange(EditorTab.CSS)}
-                                className={styles.editor}
-                                language="css"
-                                onMount={handleEditorMount}
-                                options={editorOptions}
-                            />
-                        </Tabs.TabPane>
-                        <Tabs.TabPane
-                            tab={
-                                <div className={styles.tabTitleWrapper}>
-                                    <Javascript className={styles.tabTitle} />
-                                </div>
-                            }
-                            key={EditorTab.JS}
-                            className={styles.editorTab}
-                        >
-                            <AppEditor
-                                value={sources.js}
-                                onChange={handleSourceChange(EditorTab.JS)}
-                                className={styles.editor}
-                                language="javascript"
-                                onMount={handleEditorMount}
-                                options={editorOptions}
-                            />
-                        </Tabs.TabPane>
-                    </Tabs>
-                </Col>
-                <Col
-                    span={rightColSpan}
-                    className={classNames('yui3-cssreset', styles.col, styles.rightCol, {
-                        'd-none': viewMode === ViewMode.EDITOR
-                    })}
-                >
-                    <iframe srcDoc={resultSource} className={styles.resultFrame} />
-                </Col>
-            </Row>
+            {viewMode === ViewMode.SPLIT ? (
+                <Split className={styles.containerRow} direction="horizontal" minSize={0} snapOffset={25}>
+                    {leftCol}
+                    {rightCol}
+                </Split>
+            ) : (
+                <Row className={styles.containerRow}>
+                    {viewMode === ViewMode.EDITOR && leftCol}
+                    {viewMode === ViewMode.VIEW && rightCol}
+                </Row>
+            )}
         </PageContainer>
     );
 };
