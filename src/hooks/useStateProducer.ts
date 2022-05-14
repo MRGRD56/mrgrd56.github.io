a02e129e74bd818daf41985e66310c8b0c13f09a
k.golikov <kgolikov@37bytes.com>
2022-05-14 21:41:30 +0500

    Refactor produceState

--- src/hooks/useStateProducer.ts
+++ src/hooks/useStateProducer.ts
@@ -1,7 +1,7 @@
 import { Dispatch, SetStateAction, useCallback } from 'react';
 import produceState, { StateProducerRecipe } from '../utils/produceState';
 
-const useStateProducer = <S extends object>(setState: Dispatch<SetStateAction<S>>) => {
+const useStateProducer = <S>(setState: Dispatch<SetStateAction<S>>) => {
     return useCallback(
         (recipe: StateProducerRecipe<S>) => {
             produceState(setState, recipe);
--- src/utils/produceState.ts
+++ src/utils/produceState.ts
@@ -1,19 +1,15 @@
 import { Dispatch, SetStateAction } from 'react';
 import produce, { Draft } from 'immer';
-import { Nothing } from 'immer/src/internal';
 
-type ValidRecipeReturnType<State> = State | void | undefined | (State extends undefined ? Nothing : never);
+type RecipeReturnType<S> = S | void | undefined;
+export type StateProducerRecipe<S> = <D extends Draft<S>>(draft: D) => RecipeReturnType<D>;
 
-type RecipeReturnType<State> = ValidRecipeReturnType<State> | Promise<ValidRecipeReturnType<State>>;
-
-export type StateProducerRecipe<S> = (draft: Draft<S>) => RecipeReturnType<Draft<S>>;
-
-const produceState = <S extends object>(
+const produceState = <S>(
     setState: Dispatch<SetStateAction<S>>,
     recipe: StateProducerRecipe<S>
 ): void | Promise<void> => {
     setState((state) => {
-        return produce(state, recipe as any);
+        return produce(state, recipe);
     });
 };
 
