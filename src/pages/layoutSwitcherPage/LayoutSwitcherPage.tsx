d41682fce503c94d97aa8de2724179d757e1e0fa
k.golikov <kgolikov@37bytes.com>
2022-07-30 00:14:27 +0500

    Optimize LayoutSwitcherPage

--- src/pages/layoutSwitcherPage/LayoutSwitcherPage.tsx
+++ src/pages/layoutSwitcherPage/LayoutSwitcherPage.tsx
@@ -7,38 +7,40 @@ import getLocalStorageKey from '../../utils/getLocalStorageKey';
 const EN_LAYOUT = '`1234567890-=qwertyuiop[]\\asdfghjkl;\'zxcvbnm,./~!@#$%^&*()_+QWERTYUIOP{}|ASDFGHJKL:"ZXCVBNM<>?';
 const RU_LAYOUT = 'ё1234567890-=йцукенгшщзхъ\\фывапролджэячсмитьбю.Ё!"№;%:?*()_+ЙЦУКЕНГШЩЗХЪ/ФЫВАПРОЛДЖЭЯЧСМИТЬБЮ,';
 
-const convertEnToRu: TextBiConvert = (source) => {
-    return convertInternal(source, EN_LAYOUT, RU_LAYOUT);
-};
-
-const convertRuToEn: TextBiConvert = (source) => {
-    return convertInternal(source, RU_LAYOUT, EN_LAYOUT);
-};
-
-const convertInternal = (source: string, sourceLayout: string, resultLayout: string) => {
-    if (sourceLayout.length !== resultLayout.length) {
+const mapLayouts = (sourceLayout: string, targetLayout: string): Record<string, string> => {
+    if (sourceLayout.length !== targetLayout.length) {
         throw new Error('Different length of layouts');
     }
 
-    return Array.from(source)
-        .map((char) => {
-            const index = sourceLayout.indexOf(char);
-            if (index !== -1) {
-                return resultLayout[index];
-            }
+    const result: Record<string, string> = {};
 
-            return char;
-        })
+    for (let i = 0; i < sourceLayout.length; i++) {
+        result[sourceLayout[i]] = targetLayout[i];
+    }
+
+    return result;
+};
+
+const EN_RU_MAP = mapLayouts(EN_LAYOUT, RU_LAYOUT);
+const RU_EN_MAP = mapLayouts(RU_LAYOUT, EN_LAYOUT);
+
+const switchLayout = (source: string, layoutMap: Record<string, string>): string => {
+    return Array.from(source)
+        .map((char) => layoutMap[char] ?? char)
         .join('');
 };
+
+const switchEnToRu: TextBiConvert = (source) => switchLayout(source, EN_RU_MAP);
+
+const switchRuToEn: TextBiConvert = (source) => switchLayout(source, RU_EN_MAP);
 
 const LayoutSwitcherPage: FunctionComponent = () => {
     return (
         <TextBiConverterPageContainer
             source1={{ title: 'QWERTY' }}
             source2={{ title: 'ЙЦУКЕН' }}
-            convert1to2={convertEnToRu}
-            convert2to1={convertRuToEn}
+            convert1to2={switchEnToRu}
+            convert2to1={switchRuToEn}
             swapStateStorageKey={getLocalStorageKey('layout-switcher', 'isSwapped')}
         />
     );
