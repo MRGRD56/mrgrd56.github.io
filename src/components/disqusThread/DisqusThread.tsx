c27783f06ac923ee00232b583c14ee83d9035ca8
k.golikov <kgolikov@37bytes.com>
2022-10-20 22:52:46 +0500

    Remove commented code

--- src/components/disqusThread/DisqusThread.tsx
+++ src/components/disqusThread/DisqusThread.tsx
@@ -3,18 +3,6 @@ import { DiscussionEmbed } from 'disqus-react';
 import useAppLocation from '../../hooks/useAppLocation';
 import useAppSettings from '../../hooks/useAppSettings';
 
-// const initializeDisqus = (pageUrl: string, pageIdentifier: string) => {
-//     (window as any).disqus_config = function (this: {page: {url: string, identifier: string}}) {
-//         this.page.url = pageUrl;  // Replace PAGE_URL with your page's canonical URL variable
-//         this.page.identifier = pageIdentifier; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
-//     };
-//
-//     const script = document.createElement('script');
-//     script.src = 'https://mrgrd56.disqus.com/embed.js';
-//     script.setAttribute('data-timestamp', String(Number(new Date())));
-//     (document.head || document.body).appendChild(script);
-// };
-
 type Props = Omit<HTMLProps<HTMLDivElement>, 'id'>;
 
 type DiscussionEmbedConfig = DiscussionEmbed['props']['config'];
